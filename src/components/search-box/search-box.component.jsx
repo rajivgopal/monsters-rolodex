2a7bd3a030be59bf513b621b9e6db366152efc5a
Rajiv Venugopal <rajivgopal@yahoo.com>
2019-07-28 00:43:12 +0800

    Monster rolodex

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./search-box.styles.css";
+
+const SearchBox = ({ placeHolder, handleChange }) => {
+  return (
+    <input
+      className="search"
+      type="search"
+      placeholder={placeHolder}
+      onChange={handleChange}
+    />
+  );
+};
+
+export default SearchBox;
